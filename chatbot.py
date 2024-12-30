198d0fb0f7677729fb5061457f9c160df6caaccf
Jeffrey Wu <wu19930628@gmail.com>
2024-12-30 15:34:43 +0800

    update: 使用預定義的ToolNode, tools_condition封裝調用過程

--- chatbot.py
+++ chatbot.py
@@ -2,18 +2,18 @@ from typing import Annotated
 
 from langchain_openai import ChatOpenAI
 
-from langchain_core.messages import ToolMessage
 
 from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.graph.message import add_messages
 
 import streamlit as st
 from PIL import Image
 import io
 import os
-import json
+
 from dotenv import load_dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
 
@@ -29,47 +29,21 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-class BasicNode:
-    def __init__(self, tools: list):
-        self.tools_by_name = {tool.name: tool for tool in tools}
-        logger.info(f"工具初始化: {list(self.tools_by_name.keys())}")
-
-    def __call__(self, inputs: dict):
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("無訊息")
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"]
-            )
-            outputs.append(
-                ToolMessage(
-                    content=json.dumps(tool_result),
-                    name=tool_call["name"],
-                    tool_call_id=tool_call["id"],
-                )
-            )
-        return {"messages": outputs}
-
-
 graph_builder = StateGraph(State)
 
 search_tool = TavilySearchResults(
     max_results=2,
     search_depth="advanced",
     include_answer=True,
-    include_raw_content=True,
-    include_images=True,
+    include_raw_content=False,
+    include_images=False,
     tavily_api_key=os.getenv("TAVILY_API_KEY"),
 )
 
+
 tools = [search_tool]
 
-
 llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0.25)
-
 llm_with_tools = llm.bind_tools(tools)
 
 
@@ -78,8 +52,19 @@ def chatbot(state: State):
 
 
 graph_builder.add_node("chatbot", chatbot)
+
+# 可以自定義class BasicToolNode或使用預定義的ToolNode, 封裝和抽象中間過程
+# ref : https://langchain-ai.github.io/langgraph/tutorials/introduction/#__codelineno-18-6
+graph_builder.add_node("tools", ToolNode(tools))
+
+# 可以自定義route_tool function 或直接使用tools_condition
+# ref: https://langchain-ai.github.io/langgraph/tutorials/introduction/#__codelineno-19-1
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+graph_builder.add_edge("tools", "chatbot")
+
 graph_builder.set_entry_point("chatbot")
 graph_builder.set_finish_point("chatbot")
+
 graph = graph_builder.compile()
 
 
@@ -92,7 +77,7 @@ st.image(image)
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [("user", user_input)]}):
         for value in event.values():
-            st.write("Assistant:", value["messages"][-1].content)
+            st.write(value["messages"][-1].content)
 
 
 user_input = st.text_input("輸入訊息:")
